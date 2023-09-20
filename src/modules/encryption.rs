f090bd61d5b29c80897c691722b8d162aa6ae44a
porkiedev <porkiedev@gmail.com>
2023-09-20 03:29:31 -0500

    More licensing information

--- src/modules/encryption.rs
+++ src/modules/encryption.rs
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2023 Elijah Fry
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+*/
+
+
 use argon2::{Argon2, Error, password_hash::rand_core::RngCore};
 use chacha20::ChaCha20;
 use chacha20poly1305::{
